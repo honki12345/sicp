e0ac20af63ccf4793ba0478e72192d60c73927c6
hookim <hookim@student.42gyeongsan.kr>
2025-08-11 07:57:32 +0000

    0811

--- ch01/retry.rkt
+++ ch01/retry.rkt
@@ -428,11 +428,11 @@
 
 ;; ex1.33
 
-(define (accumulate combiner null-value term a next b)
-  (if (> a b)
-      null-value
-      (combiner (term a)
-                (accumulate combiner null-value term (next a) next b))))
+;; (define (accumulate combiner null-value term a next b)
+;;   (if (> a b)
+;;       null-value
+;;       (combiner (term a)
+;;                 (accumulate combiner null-value term (next a) next b))))
 (define (filtered-accumulate predicate combiner null-value term a next b)
   (if (> a b)
       null-value
@@ -440,3 +440,121 @@
           (combiner (term a)
                     (filtered-accumulate predicate combiner null-value term (next a) next b))
           (filtered-accumulate predicate combiner null-value term a next b))))
+
+
+(define (pi-sum2 a b)
+  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+       a
+       (lambda (x) (+ x 4))
+       b))
+
+(define (integral2 f a b dx)
+  (* (sum f
+          (+ a (/ dx 2.0))
+          (lambda (x) (+ x dx))
+          b)
+     dx))
+
+;; (define (f x y)
+;;   (let ((a (+ 1 (* x y)))
+;;         (b (- 1 y)))
+;;     (+ (* x (square a))
+;;        (* y b)
+;;        (* a b))))
+
+
+
+;; ex1-34
+
+(define (f g)
+  (g 2))
+(f square)
+(f (lambda (z) (* z (+ z 1))))
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value)
+                 (search f neg-point midpoint))
+                ((negative? test-value)
+                 (search f midpoint pos-point))
+                (else midpoint))))))
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "Value are not of opposite sign" a b)))))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (sqrt3 x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+;; ex1-35
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
+;; ex1.36
+
+(define (fixed-point2 f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display guess)
+      (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (display "start fix-point")
+  (newline)
+  (try first-guess))
+
+;; (fixed-point2 (lambda (x) (+ 1 (/ 1 x))) 1.0)
+(fixed-point2 (lambda (x) (/ (log 1000) (log x))) 2.0)
+
+;; ex1-37
+(display " expected")
+(/ 1 (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+(define (cont-frac k)
+  (define (cont-frac-recurs n d count)
+    (if (> count k)
+        0
+        (/ (n count)
+           (+ (d count)
+              (cont-frac-recurs n d (+ count 1))))))
+  (cont-frac-recurs (lambda (i) 1.0)
+                    (lambda (i) 1.0)
+                    1))
+
+(define (cont-frac2 k)
+  (define (cont-frac-iter n d count value)
+    (if (= count 0)
+        value
+        (cont-frac-iter n d (- count 1) (/ (n count)
+                                           (+ (d count) value)))))
+  (cont-frac-iter (lambda (i) 1.0)
+                  (lambda (i) 1.0)
+                  k
+                  0))
+
+(cont-frac 11)
+(cont-frac2 11)
